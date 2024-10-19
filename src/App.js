c96692eba81772f827247a5dd6551db13504bac1
ahmed <ahmed.zaakan@gmail.com>
2024-10-20 00:01:34 +0300

    Add NavBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react'
-import './App.css'
+import './App.css';
+import React from 'react';
+import { createTheme, ThemeProvider   } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import NavBar from './component/NavBar/NavBar';
+
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Cairo',
+      'Tajawal',
+    ].join(','),
+  },
+});
 
 const App = () => {
   return (
-    <div>
-      <h1>السل عليكم ورحمة الله ام</h1>
-    </div>
-  )
+    <ThemeProvider theme={theme}>
+      <NavBar />
+      <Typography variant="h5">السلام عليكم ورحمة الله وبركاته</Typography>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
